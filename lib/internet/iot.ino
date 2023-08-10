7589ae0d22e6466d06b5f27a18a0693852baf1a2
zhoushoujian <zhoushoujianwork@163.com>
2023-08-10 23:14:29 +0800

    add mqtt for ali iot

--- lib/internet/iot.ino
+++ lib/internet/iot.ino
@@ -1,1 +1,76 @@
-// 使用物联网模块实现云端数据交互
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+// WiFi 相关设置
+const char *ssid = "xiaomi-5-505";
+const char *password = "mkonjibhu";
+
+// {"clientId":"gt1glQ2SjlH.device1|securemode=2,signmethod=hmacsha256,timestamp=1691679592328|","username":"device1&gt1glQ2SjlH","mqttHostUrl":"iot-06z00irsh30z306.mqtt.iothub.aliyuncs.com","passwd":"9591edd43cb2e26ddadf841bb62127383ecc27022f9b651bfe518fc96ae0af28","port":1883}
+// MQTT 相关设置
+const char *mqttServer = "iot-06z00irsh30z306.mqtt.iothub.aliyuncs.com";
+const int mqttPort = 1883;
+const char *mqttUsername = "device1&gt1glQ2SjlH";
+const char *mqttPassword = "3db274762ce91d5df7e709128bb36f33b7c105fbb388d4b6635e9b12f644efe5";
+const char *mqttClientId = "gt1glQ2SjlH.device1|securemode=2,signmethod=hmacsha256,timestamp=1691679515452|";
+const char *mqttTopic = "/sys/a13FN5TplKq/mqtt_basic_demo/thing/event/+/post_reply";
+
+WiFiClient wifiClient;
+PubSubClient mqttClient(wifiClient);
+
+void setupWiFi()
+{
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(100);
+        Serial.println("Connecting to WiFi...");
+    }
+
+    Serial.println("Connected to WiFi");
+    Serial.println(WiFi.localIP());
+}
+
+void callback(char *topic, byte *payload, unsigned int length)
+{
+    Serial.print("Received message: ");
+    for (int i = 0; i < length; i++)
+    {
+        Serial.print((char)payload[i]);
+    }
+    Serial.println();
+}
+
+void setupIot()
+{
+    setupWiFi();
+
+    mqttClient.setServer(mqttServer, mqttPort);
+    mqttClient.setCallback(callback);
+}
+
+void loopIot()
+{
+    if (!mqttClient.connected())
+    {
+        Serial.println("Connecting to MQTT...");
+
+        if (mqttClient.connect(mqttClientId, mqttUsername, mqttPassword))
+        {
+            Serial.println("Connected to MQTT");
+
+            mqttClient.subscribe(mqttTopic);
+        }
+        else
+        {
+            Serial.print("Failed to connect to MQTT, rc=");
+            Serial.print(mqttClient.state());
+            Serial.println(" retrying in 5 seconds...");
+
+            delay(5000);
+        }
+    }
+    Serial.printf("loopIot\n");
+
+    mqttClient.loop();
+}
--- src/main.ino
+++ src/main.ino
@@ -2,12 +2,14 @@
 #include <lvgl.h>
 #include <TaskScheduler.h>
 #include <L76X.ino>
+#include <iot.ino>
 // #include <wifi.ino>
 
 // 创建任务调度器对象
 Scheduler taskScheduler;
 Task t1(20, TASK_FOREVER, &loop_gy91_mid);
 Task t2(0, TASK_FOREVER, &LoopL76X);
+Task t3(0, TASK_FOREVER, &loopIot);
 
 void setup()
 {
@@ -15,10 +17,12 @@ void setup()
   dashboardInit();
   setupGy91();
   setupL76X();
+  setupIot();
   // setupWifi();
   taskScheduler.init();
   taskScheduler.addTask(t1);
   taskScheduler.addTask(t2);
+  taskScheduler.addTask(t3);
   taskScheduler.enableAll();
   Serial.println("Setup done");
 }
