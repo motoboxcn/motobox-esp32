3abc037b307325aa0d0a9963062764bd21b72579
zhoushoujian <zhoushoujianwork@163.com>
2024-03-06 20:44:53 +0800

    chore:esp32 lvgl success

--- src/main.ino
+++ src/main.ino
@@ -1,30 +1,34 @@
 #include <gy91.ino>
 #include <lvgl.h>
 #include <TaskScheduler.h>
-#include <L76X.ino>
+#include <dashboard.ino>
+// #include <L76X.ino>
 // #include <wifi.ino>
 
 // 创建任务调度器对象
 Scheduler taskScheduler;
 Task t1(20, TASK_FOREVER, &loop_gy91_mid);
-Task t2(0, TASK_FOREVER, &LoopL76X);
+// Task t2(0, TASK_FOREVER, &LoopL76X);
 
 void setup()
 {
   Serial.begin(115200);
+  Serial.println("Start setup");
   dashboardInit();
-  setupGy91();
-  setupL76X();
+  // setupGy91();
+  // setupL76X();
   // setupWifi();
   taskScheduler.init();
   taskScheduler.addTask(t1);
-  taskScheduler.addTask(t2);
-  taskScheduler.enableAll();
+  // taskScheduler.addTask(t2);
+  // taskScheduler.enableAll();
   Serial.println("Setup done");
 }
 
 void loop()
 {
+  lv_timer_handler();
   taskScheduler.execute();
-  // LoopL76X();
+  // print gy91
+  // Serial.println(bmp.readTemperature());
 }
